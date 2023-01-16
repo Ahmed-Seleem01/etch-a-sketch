7d8c926abbf8890f21235dc7bccf5c391fb22a16
Ahmed-Seleem01 <ahmedseleem.c@gmail.com>
2023-01-16 23:16:04 +0200

    Draw the 16x16 grid when the page first load

--- js/app.js
+++ js/app.js
@@ -4,3 +4,17 @@ value.textContent = input.value + 'x' + input.value;
 input.addEventListener("input", (event) => {
   value.textContent = event.target.value + 'x' + event.target.value;
 });
+
+drawGrid();
+function drawGrid(gridNumber = 16){
+  const container = document.querySelector('#container');
+  let totalGridNumber = Math.floor(gridNumber * gridNumber);
+  console.log(gridNumber);
+  container.textContent = '';
+  for(let i =0; i < totalGridNumber; i++){
+    let div = document.createElement('div');
+    div.classList.add('grid');
+    div.setAttribute('style', `width: ${100/gridNumber}%; height: ${100/gridNumber}%;`)
+    container.appendChild(div);
+  }
+}
